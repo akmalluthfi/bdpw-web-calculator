5f183298facb8a43bb5e43df922a35e55944f36d
akmalluthfi <akmalluthfi19@gmail.com>
2022-06-25 00:11:30 +0700

    add clear and delete button

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+// create object to keep state
 const calculator = {
   firstNum: null,
   lastNum: null,
@@ -8,27 +9,64 @@ const calculator = {
   result: null,
 };
 
-// ambil semua button dan beri event
+// get all buttons and give event onClick
 const buttons = document.querySelectorAll('.button');
 buttons.forEach((button) => {
   button.addEventListener('click', function () {
-    // cek apakah click angka
+    // when the number button is clicked
     if (this.classList.contains('num')) return handleNum(this);
-    // cek apakah click operator
+    // when the operator button is clicked
     if (this.classList.contains('operator')) return handleOperator(this);
-    // cek apakah click sama dengan
+    // when the equals button is clicked
     if (this.classList.contains('equals')) return performCalculate();
+    // when the clear button is clicked
+    if (this.classList.contains('clear')) return clearDisplay();
+    // when the delete button is clicked
+    if (this.classList.contains('delete')) return handleDelete();
   });
 });
 
+function handleDelete() {
+  // cek apakah operator null atau tidak
+  if (calculator.operator === null) {
+    if (calculator.firstNum === null) return;
+    calculator.firstNum = calculator.firstNum.slice(0, -1);
+  } else {
+    calculator.lastNum = calculator.lastNum.slice(0, -1);
+  }
+
+  if (calculator.firstNum.length === 0) calculator.firstNum = null;
+  updateMainDisplay();
+}
+
+function reset() {
+  calculator.firstNum = null;
+  calculator.lastNum = null;
+  calculator.operator = null;
+  calculator.isOperatorSet = false;
+  calculator.mainContent = '';
+  calculator.secondContent = '';
+  calculator.result = null;
+}
+
+function clearDisplay() {
+  reset();
+  const second = document.getElementById('second');
+  second.style.visibility = 'hidden';
+  updateMainDisplay();
+}
+
 function performCalculate() {
   if (calculator.result !== null && !calculator.isOperatorSet) return;
+
+  if (calculator.operator === null) return;
 
   if (calculator.lastNum == null) {
     calculator.lastNum = '0';
     calculator.mainContent += calculator.lastNum;
   }
 
+  // change string to float
   const first = parseFloat(calculator.firstNum);
   const last = parseInt(calculator.lastNum);
 
@@ -56,33 +94,20 @@ function updateSecondDisplay() {
 }
 
 function handleOperator(button) {
-  // cek apakah firstNum null
-  // return false
   if (calculator.firstNum === null) return;
   calculator.isOperatorSet = true;
-  // cek operator apa yang ditekan
-  if (button.classList.contains('addition')) {
-    calculator.operator = '+';
-  }
 
-  if (button.classList.contains('subtraction')) {
-    calculator.operator = '-';
-  }
-
-  if (button.classList.contains('multiplication')) {
-    calculator.operator = '*';
-  }
-
-  if (button.classList.contains('divide')) {
-    calculator.operator = '/';
-  }
+  if (button.classList.contains('addition')) calculator.operator = '+';
+  if (button.classList.contains('subtraction')) calculator.operator = '-';
+  if (button.classList.contains('multiplication')) calculator.operator = '*';
+  if (button.classList.contains('divide')) calculator.operator = '/';
 
   updateMainDisplay();
 }
 
 function updateMainDisplay() {
   const mainDisplay = document.getElementById('main');
-  const first = calculator.firstNum ?? '';
+  const first = calculator.firstNum ?? '0';
   const operator = calculator.operator ?? '';
   const last = calculator.lastNum ?? '';
 
@@ -91,7 +116,6 @@ function updateMainDisplay() {
 }
 
 function handleNum(button) {
-  // cek apakah
   if (calculator.isOperatorSet) {
     if (calculator.lastNum === null) {
       calculator.lastNum = button.innerText;
@@ -99,7 +123,6 @@ function handleNum(button) {
       calculator.lastNum += button.innerText;
     }
   } else {
-    // result sudah ada dan operator belum diset
     if (calculator.result !== null) {
       calculator.firstNum = button.innerText;
       calculator.result = null;
@@ -111,6 +134,6 @@ function handleNum(button) {
       }
     }
   }
-  // update main display
+
   updateMainDisplay();
 }
